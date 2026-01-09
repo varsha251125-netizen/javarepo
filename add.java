7f9a9e2992530d9a9c3967492600e4205bbb23d6
varsha6659 <varsha251125@gmail.com>
2026-01-09 10:53:22 +0530

    hello

---
+++ add.java
@@ -0,0 +1,13 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int number1 = 10;
+        int number2 = 20;
+
+        // Add the two numbers
+        int sum = number1 + number2;
+
+        // Print the result to the console
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
